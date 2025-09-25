1d7a90f0ada024021df9eb758b5a01ad4634836d
Deler <del2547.pv@gmail.com>
2025-09-25 15:50:26 +0700

    test gemini

--- src/services/gemini.ts
+++ src/services/gemini.ts
@@ -1,4 +1,4 @@
-import { GoogleGenAI } from "@google/genai";
+import { GoogleGenAI } from '@google/genai';
 import type { Context } from 'hono'
 
 const ai = new GoogleGenAI({ apiKey: process.env.API_KEY! });
@@ -7,7 +7,7 @@ export const genAI = async (c: Context) => {
   try {
       const response = await ai.models.generateContent({
       model: "gemini-2.5-flash",
-      contents: "Explain how AI works in a few words",
+      contents: "Help me generate words for city pop or synthpop album cover  ",
     });
     console.log(response.text);
     return c.json({ message: "Genmini generate", response: response.text });
