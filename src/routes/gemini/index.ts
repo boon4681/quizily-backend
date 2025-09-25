783dbe7479a54cf05ca4a3145fa3a82d3b35e479
Deler <del2547.pv@gmail.com>
2025-09-25 22:28:25 +0700

    test gemini generateQuiz

--- src/routes/gemini/index.ts
+++ src/routes/gemini/index.ts
@@ -1,9 +1,10 @@
 import { Hono } from 'hono'
-import { genAI } from '../../services/gemini.js'
+import { genAI, generateQuiz } from '../../services/gemini.js'
 
 
 const app = new Hono()
 
 app.get("/gemini/*", genAI);
+app.get("/generateQuiz/*", generateQuiz);
 
 export {app as Gemini}
--- src/services/gemini.ts
+++ src/services/gemini.ts
@@ -1,4 +1,4 @@
-import { GoogleGenAI } from '@google/genai';
+import { GoogleGenAI, Type } from '@google/genai';
 import type { Context } from 'hono'
 
 const ai = new GoogleGenAI({ apiKey: process.env.API_KEY! });
@@ -16,4 +16,58 @@ export const genAI = async (c: Context) => {
     return c.json({ message: "Internal server error" });
   }
   
+}
+
+export const generateQuiz = async (c: Context) => {
+
+  try {
+    const pdfResp = await fetch('https://discovery.ucl.ac.uk/id/eprint/10089234/1/343019_3_art_0_py4t4l_convrt.pdf')
+        .then((response) => response.arrayBuffer());
+
+    const contents = [
+        { text: "Summarize this document and make a simple (easy) quiz for 5 quiz with 4 multiple choices and give a correct anwser by return correct anwser in response type (correct) follow with config" },
+        {
+            inlineData: {
+                mimeType: 'application/pdf',
+                data: Buffer.from(pdfResp).toString("base64")
+            }
+        }
+    ];
+
+    const response = await ai.models.generateContent({
+        model: "gemini-2.5-flash",
+        contents: contents,
+        config: {
+          responseMimeType: 'application/json',
+          responseSchema: {
+            type: Type.ARRAY,
+            items: {
+              type: Type.OBJECT,
+              properties: {
+                question: {
+                  type: Type.STRING,
+                },
+                answers: {
+                  type: Type.ARRAY,
+                  items: {
+                    type: Type.STRING,
+                  },
+                },
+                correct: {
+                  type: Type.STRING,
+                },
+              },
+              propertyOrdering: ["question", "answers", "correct"],
+            },
+          },
+        }   
+    });
+    console.log(response.text);
+    return c.json({ message: "Genmini generate quiz", response: response.text });
+
+  }
+  catch (error) {
+    console.error(error);
+    return c.json({ message: "Internal server error" });
+  }
 }
