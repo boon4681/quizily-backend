64d0ca38ffdc472ed5a05da104eae7d88c97bd43
Deler <del2547.pv@gmail.com>
2025-10-31 22:27:36 +0700

    add generate quiz link feature

--- src/quizcontroller/index.ts
+++ src/quizcontroller/index.ts
@@ -1,5 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 import type { Context } from "hono";
+import { nanoid } from 'nanoid';
+import { success } from "zod";
 
 const prisma = new PrismaClient();
 
@@ -36,3 +38,46 @@ export const makeQuiz = async (c: Context) => {
     return c.json({ message: "Internal server error" });
   }
 };
+
+export const getQuizLink = async (c: Context) => {
+  const quizId = c.req.param('id');
+
+  const shareToken = nanoid(10);
+
+  const quiz = await prisma.quiz.update({
+    where: { id: quizId },
+    data: { shareToken }
+  });
+
+  const shareUrl = `http://localhost:3000/share/${shareToken}`;
+
+  return c.json({success: true, shareUrl});
+}
+
+export const getQuiz = async (c: Context) => {
+
+  try {
+    const token = c.req.param('token');
+    const quiz = await prisma.quiz.findFirst({
+      where: {shareToken: token},
+      select: {
+        id: true,
+        title: true,
+        description: true,
+        questions: true
+      }
+    })
+    if (!quiz) {
+      return c.json({ error: 'Invalid or expired link' }, 404)
+    }
+
+    return c.json({
+      success: true,
+      quiz,
+    })
+  } catch (err) {
+    console.error(err)
+    return c.json({ error: 'Server error' }, 500)
+  }
+
+}
--- src/routes/quiz/index.ts
+++ src/routes/quiz/index.ts
@@ -1,9 +1,13 @@
 
 import { Hono } from 'hono'
-import { makeQuiz } from '../../quizcontroller/index.js';
+import { getQuiz, makeQuiz } from '../../quizcontroller/index.js';
+import { getQuizLink } from '../../quizcontroller/index.js';
 
 const app = new Hono()
 
-app.post("/quiz/*", makeQuiz)
+app.post("/quiz", makeQuiz)
+app.post("/quiz/:id/share", getQuizLink)
+
+app.get ("/quiz/share/:token", getQuiz)
 
 export {app as APIQuiz}
